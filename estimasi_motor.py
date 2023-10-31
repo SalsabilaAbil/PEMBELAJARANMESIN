7f96917990db691fe9634c37c200ca24ee1e1f4e
SalsabilaAbil <149142675+SalsabilaAbil@users.noreply.github.com>
2023-10-31 10:43:05 +0700

    estimasi_motor.py

--- estimasi_motor.py
+++ estimasi_motor.py
@@ -8,10 +8,11 @@ st.title('Estimasi Keuntungan Penjualan Motor Di Eropa')
 Year = st.number_input('Input Tahun Motor')
 Unit_Cost = st.number_input('Input Biaya Unit')
 Order_Quantity = st.number_input('Input Jumlah Pesanan')
+Day = st.number_input('Input Day')
 
 prediksi = ''
 if st.button('Estimasi Jumlah Keuntungan'):
     prediksi = model.predict(
-        [[Year, Unit_Cost, Order_Quantity]]
+        [[Year, Unit_Cost, Order_Quantity, Day]]
     )
     st.write('Estimasi Keuntungan Penjualan Motor di Eropa : ', prediksi)
